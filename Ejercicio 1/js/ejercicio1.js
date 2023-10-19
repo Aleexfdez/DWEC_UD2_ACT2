91d06860097cb8f17ca222bca0ced0a093ca2ed3
Aleexfdez <alexfernandez567@gmail.com>
2023-10-19 16:46:08 +0200

    1 funcionando

---
+++ Ejercicio 1/js/ejercicio1.js
@@ -0,0 +1,19 @@
+function separarNumeros(){
+    
+    var numero = 1;
+    var guardarNumeros = [];
+
+    parseInt(numero);
+    for(var i=0; i<arguments.length;i++){
+        if(typeof(arguments[i])==typeof(numero)){
+            guardarNumeros.push(arguments[i]);
+        }
+    }
+    return guardarNumeros;
+}
+
+console.log(separarNumeros("Hola"));
+console.log(separarNumeros("Hola", "Adiós"));
+console.log(separarNumeros("Hola", "Adiós", 123));
+console.log(separarNumeros("Hola", "Adiós", undefined, 22222));
+console.log(separarNumeros("Hola", "Adiós", 123, "22222", 7893838, new Date()));
